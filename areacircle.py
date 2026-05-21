eba8e4597cc11968419fb63e9a8af2c7754caa14
Sela456 <136029487+Sela456@users.noreply.github.com>
2026-05-21 15:21:44 +0200

    added other shapes

--- areacircle.py
+++ areacircle.py
@@ -1,4 +1,26 @@
+print("Enter shape whose area is to be calculated: circle, square, rectangle, triangle")
+shape = str(input("enter shape: "))
 
-radius = float(input("Enter radius of circle: "))
-area = (3.14 * radius ** 2)
-print("area of circle is: ", round(area,2) ) 
+if shape == "circle":
+    radius = float(input("enter radius of circle: "))
+    area = 3.14 * radius ** 2    
+    print("area of circle is: ", round(area,2))
+
+elif shape ==  "square":
+    length = int(input("enter length of square: "))
+    area = length * length
+    print("area of square is :", area)
+
+elif shape == "rectangle":
+    length = int(input("enter length of rectangle: "))
+    breath = int(input("enter breath of rectangle: "))
+    area = length * breath
+    print("area of rectangle is: ", area)
+
+elif shape == "triangle":
+    length = int(input("enter length of triangle: "))
+    breath = int(input("enter breath of triangle: "))
+    area = 0.5 * length* breath
+    print("area of triangle is: ", area)
+    
+ 
