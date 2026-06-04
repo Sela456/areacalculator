6135d77a40909e2e136b8d92f924c8e114b8894d
Sela456 <136029487+Sela456@users.noreply.github.com>
2026-06-04 11:46:03 +0000

    deleted-textfile

--- areacircle.py
+++ areacircle.py
@@ -1,10 +1,12 @@
 print("Enter shape whose area is to be calculated: circle, square, rectangle, triangle")
 shape = str(input("enter shape: "))
 
+
 if shape == "circle":
     radius = float(input("enter radius of circle: "))
     area = 3.14 * radius ** 2    
     print("area of circle is: ", round(area,2))
+
 
 elif shape ==  "square":
     length = int(input("enter length of square: "))
