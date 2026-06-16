5bad5dff41dbe3fea17654bdde075954d7baba82
Sela456 <136029487+Sela456@users.noreply.github.com>
2026-06-16 09:02:52 +0000

    update yaml file

--- areacircle.py
+++ areacircle.py
@@ -17,7 +17,8 @@ def triangle_area(length, breadth):
 
 
 def main():
-    print("Enter shape whose area is to be calculated: circle, square, rectangle, triangle")
+    print("Enter shape whose area is to be calculated:" 
+          " circle, square, rectangle, triangle")
     shape = input("enter shape: ").lower()
 
     if shape == "circle":
